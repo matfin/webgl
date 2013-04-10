01571be106aa6dbf972ca112ec78b2f525c53152
matfin <matfin@gmail.com>
2013-04-10 21:24:38 +0100

    Deleted old webgl code, starting again with an example I know works (bold I know)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-var GL;
-var Texture;
-
-$('document').ready(function(){
-    GL = new WebGL('glcanvas', 'FragmentShader', 'VertexShader');
-    TextureImage = new Image();
-    TextureImage.onload = function(){
-        Texture = GL.LoadTexture(TextureImage);
-        GL.Draw(Cube, Texture);
-    };
-    TextureImage.src = '/assets/textures/Texture.png';
-});
+onload = function(){
+    console.log('Body loaded!');
+}
