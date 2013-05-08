b1b209a4da2d8419b8f1eb6cf9ce0e60e2ed1ce4
Matt Finucane <matfin@gmail.com>
2013-05-08 23:10:18 +0100

    Added .htaccess to load different experiments. Also added new landing page background using compass css gradients.

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
+<?php
+    define('page', $_REQUEST['page']);
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <?php 
-            include_once('cube/meta.php'); 
+            include_once(page . '/meta.php'); 
         ?>
         <script type="text/javascript" src="/assets/js/glMatrix-0.9.5.min.js"></script>
         <script type="text/javascript" src="/assets/js/webgl-debug.js"></script>
         <?php 
-            include_once('cube/scripts.php');
-            include_once('cube/shaders/fragment.php'); 
-            include_once('cube/shaders/vertex.php');
+            if(strcmp(page, 'landing') != 0){
+                include_once(page . '/scripts.php');
+                include_once(page . '/shaders/fragment.php'); 
+                include_once(page . '/shaders/vertex.php');
+            }
         ?>
     </head>
     <body>
         <section class="container">
-            <?php include_once('cube/content.php'); ?>
+            <?php include_once(page . '/content.php'); ?>
         </section>
     </body>
 </html>
