b11a773d17ca916785f479c4abbe49fc5c470bf7
Matt Finucane <matfin@gmail.com>
2013-05-08 22:24:08 +0100

    Abstracting and refactoring some of the CSS to allow different CSS for each experiment

---
+++ landing/meta.php
@@ -0,0 +1,8 @@
+<title>
+    WebGL Experiments
+</title>
+<meta charset="UTF-8" />
+<meta name="description" content="WebGL Experimentation">
+<meta name="keywords" content="webgl, javascript, experiments">
+<meta name="author" content="Matt Finucane">
+<link rel="stylesheet" type="text/css" href="assets/css/stylesheets/cube.css" />
